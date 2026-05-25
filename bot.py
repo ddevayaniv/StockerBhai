5833214425e3432726fcf407a674a425c26b426d
ddevayaniv <office.devayani@gmail.com>
2026-05-25 18:04:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -241,4 +241,19 @@ def main():
     save_json(STATUS_FILE, status)
 
 if __name__ == "__main__":
-    send_message("TEST MESSAGE")
+    import requests
+    import os
+
+    TOKEN = os.environ["TELEGRAM_TOKEN"]
+    CHAT_ID = os.environ["CHAT_ID"]
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": "DIRECT TELEGRAM TEST"
+    }
+
+    r = requests.post(url, json=payload)
+
+    print(r.text)
